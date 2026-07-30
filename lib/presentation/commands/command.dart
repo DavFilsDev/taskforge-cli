dbbc6647ffe1e8e518a2ff21495689c2bd39bef0
DavFilsDev <miharisoadavidfils@gmail.com>
2026-07-30 11:58:58 +0300

    feat(presentation): add Command interface

---
+++ lib/presentation/commands/command.dart
@@ -0,0 +1,10 @@
+import '../../core/utils/constants.dart';
+import '../validators/cli_input_validator.dart';
+
+abstract interface class Command {
+  String get name;
+
+  String get summary;
+
+  Future<int> execute(CliInputValidator input);
+}
