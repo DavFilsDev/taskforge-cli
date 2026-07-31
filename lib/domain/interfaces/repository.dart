17caf7908b0de257fe6f46418ddfe71a8117977c
DavFilsDev <miharisoadavidfils@gmail.com>
2026-07-31 18:21:20 +0300

    refactor(domain): make Repository<T> a pure interface with no default bodies

--- lib/domain/interfaces/repository.dart
+++ lib/domain/interfaces/repository.dart
@@ -1,11 +1,11 @@
-interface class Repository<T> {
-  Future<List<T>> getAll() => throw UnimplementedError();
+abstract interface class Repository<T> {
+  Future<List<T>> getAll();
 
-  Future<T?> getById(String id) => throw UnimplementedError();
+  Future<T?> getById(String id);
 
-  Future<void> add(T item) => throw UnimplementedError();
+  Future<void> add(T item);
 
-  Future<void> update(T item) => throw UnimplementedError();
+  Future<void> update(T item);
 
-  Future<void> deleteById(String id) => throw UnimplementedError();
+  Future<void> deleteById(String id);
 }
